7f505d58abb4cbec4f8402c842a7305c57bbae17
arvind <arvindk906@gmail.com>
2023-06-28 23:29:19 +0530

    arithmetic calculator

---
+++ src/com/project/ArithmeticCalculator.java
@@ -0,0 +1,15 @@
+package com.project;
+
+public class ArithmeticCalculator {
+    public static void main(String[] args) {
+        int a =10,b=20,result;
+        result = a+b;
+        System.out.println("addition of a and b "+result);
+        result = b - a;
+        System.out.println("substraction of a and b "+result);
+        result = a*b;
+        System.out.println("multiplication of a and b "+result);
+        result = a/b;
+        System.out.println("division of a and b "+result);
+    }
+}
